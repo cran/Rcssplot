57cc8df4a24512465b072e293cd54d9ba0beb3b5
Tomasz Konopka <tokonopka@gmail.com>
2024-11-10 17:50:02 +0000

    version 1.1.0

--- inst/doc/Rcssplot.R
+++ inst/doc/Rcssplot.R
@@ -1,4 +1,4 @@
-## ----echo = FALSE--------------------------------------------------------
+## ----showrcss, echo = FALSE---------------------------------------------------
 library("knitr")
 opts_chunk$set(fig.pos = "", fig.align = "center")
 
@@ -20,17 +20,17 @@ showrcss = function(f, remove=4) {
   cat(flines)
 }
 
-## ------------------------------------------------------------------------
+## ----basebarfun_1-------------------------------------------------------------
 a <- setNames(c(35, 55, 65, 75, 80, 80), letters[1:6])
 a
 
-## ----basebar_1A, eval=FALSE----------------------------------------------
+## ----basebar_1A, eval=FALSE---------------------------------------------------
 #  barplot(a, main="Base graphics")
 
-## ----basebar_1B, eval=FALSE----------------------------------------------
+## ----basebar_1B, eval=FALSE---------------------------------------------------
 #  barplot(a, main="Manual tuning", ylab="y label", col="#000080", border=NA, space=0.35)
 
-## ----basebarfun_2, eval = FALSE------------------------------------------
+## ----basebarfun_2, eval = FALSE-----------------------------------------------
 #  ## helper builds a string describing a range
 #  range.string <- function(x) {
 #      paste0("Data range is [", min(x), ", ", max(x), "]")
@@ -50,12 +50,13 @@ a
 #    mtext(ylab, side=2, cex=0.8, line=3, col="#444444")
 #  }
 
-## ----basebar_1C, eval=FALSE----------------------------------------------
+## ----basebar_1C, eval=FALSE---------------------------------------------------
 #  base.barplot.1(a)
 
 ## ----base_barplot, echo = FALSE, out.width = "0.9\\textwidth", out.height = "0.225\\textwidth", fig.width = 10, fig.height = 2.5, fig.cap = "Charts created with base graphics using: (A) R's {\\tt barplot} function and default settings; (B) R's {\\tt barplot} function and custom settings; (C) a custom plot function that styles bars, axes, and labels individually. "----
 par(mfrow = c(1, 3), mar = c(2, 6, 4.8, 2))
-
+a <- setNames(c(35, 55, 65, 75, 80, 80), letters[1:6])
+a
   ## helper builds a string describing a range
   range.string <- function(x) {
       paste0("Data range is [", min(x), ", ", max(x), "]")
@@ -82,26 +83,26 @@ base.barplot.1(a)
 text(-2, 104, "C", cex=2, xpd=TRUE)
 par(mfrow = c(1, 1))
 
-## ----library, eval=TRUE, echo=TRUE, cache=TRUE, comment=NA---------------
+## ----library, eval=TRUE, echo=TRUE, cache=TRUE, comment=NA--------------------
 library(Rcssplot)
 
-## ----barrcss_1, eval=TRUE, echo = FALSE, comment=NA----------------------
+## ----barrcss_1, eval=TRUE, echo = FALSE, comment=NA---------------------------
 showrcss("Rcss/vignettes.bar1.Rcss")
 
-## ----barrcss_load1, eval = TRUE------------------------------------------
+## ----barrcss_load1, eval = TRUE-----------------------------------------------
 style1 <- Rcss(file.path("Rcss", "vignettes.bar1.Rcss"))
 
-## ----barrcss_show, eval = TRUE-------------------------------------------
+## ----barrcss_show, eval = TRUE------------------------------------------------
 style1
 printRcss(style1, "barplot")
 
-## ----rcssbar_2A, eval=FALSE----------------------------------------------
+## ----rcssbar_2A, eval=FALSE---------------------------------------------------
 #  barplot(a, main="Rcssbarplot, unstyled", ylab="y label")
 
-## ----rcssbar_2B, eval=FALSE----------------------------------------------
+## ----rcssbar_2B, eval=FALSE---------------------------------------------------
 #  barplot(a, main="Rcssbarplot, styled", ylab="y label", Rcss=style1)
 
-## ----rcssbar_2C, eval=FALSE----------------------------------------------
+## ----rcssbar_2C, eval=FALSE---------------------------------------------------
 #  barplot(a, main="Rcssbarplot, override", ylab="y label", space=1, Rcss=style1)
 
 ## ----rcss_barplot, echo = FALSE, out.width = "0.9\\textwidth", out.height = "0.225\\textwidth", fig.width = 10, fig.height = 2.5, fig.cap = "Charts created with Rcssplot using: (A) the default style; (B) a style determined through a style sheet; (C) a style sheet, but with the bar width over-ridden by a setting within a function call. "----
@@ -126,10 +127,10 @@ barplot(a, main="Rcssbarplot, override", ylab="y label", space=1, Rcss=style1)
 text(-2, 104, "C", cex=2, xpd=TRUE)
 par(mfrow = c(1, 1))
 
-## ----barrcss_2, eval=TRUE, echo = FALSE, comment=NA----------------------
+## ----barrcss_2, eval=TRUE, echo = FALSE, comment=NA---------------------------
 showrcss("Rcss/vignettes.bar2.Rcss")
 
-## ----rcssbarfun_1, eval = FALSE------------------------------------------
+## ----rcssbarfun_1, eval = FALSE-----------------------------------------------
 #  ## barplot using Rcssplot, version 1
 #  rcss.barplot.1 <- function(x, main="Custom Rcss plot", ylab="y label",
 #                             Rcss="default", Rcssclass=c()) {
@@ -143,18 +144,18 @@ showrcss("Rcss/vignettes.bar2.Rcss")
 #    mtext(ylab, side=2, Rcss=Rcss, Rcssclass=c(Rcssclass,"ylab"))
 #  }
 
-## ----rcssbar_3A, eval=FALSE----------------------------------------------
+## ----rcssbar_3A, eval=FALSE---------------------------------------------------
 #  style2 <- Rcss(file.path("Rcss", c("vignettes.bar1.Rcss", "vignettes.bar2.Rcss")))
 #  rcss.barplot.1(a, main="Rcss style2", Rcss=style2)
 
-## ----barrcss_3, eval=TRUE, echo = FALSE, comment=NA----------------------
+## ----barrcss_3, eval=TRUE, echo = FALSE, comment=NA---------------------------
 showrcss("Rcss/vignettes.bar3.Rcss")
 
-## ----rcssbar_3B, eval=FALSE----------------------------------------------
+## ----rcssbar_3B, eval=FALSE---------------------------------------------------
 #  style3 <- Rcss(paste0("Rcss/vignettes.bar", c(1, 2, 3), ".Rcss"))
 #  rcss.barplot.1(a, main="Rcss style3, class typeB", Rcss=style3, Rcssclass="typeB")
 
-## ----rcssbar_3C, eval=FALSE----------------------------------------------
+## ----rcssbar_3C, eval=FALSE---------------------------------------------------
 #  rcss.barplot.1(a, main="Rcss style3, class typeC", Rcss=style3, Rcssclass="typeC")
 
 ## ----rcss_custombar, echo = FALSE, out.width = "0.9\\textwidth", out.height = "0.225\\textwidth", fig.width = 10, fig.height = 2.5, fig.cap = "Charts created by custom plot functions with base graphics and Rcssplot using: (A) a style determined by css; (B) a style sub-class defined in css; (C) a style sub-class that is not defined in css (equivalent to (A)). "----
@@ -184,7 +185,7 @@ rcss.barplot.1(a, main="Rcss style3, class typeC", Rcss=style3, Rcssclass="typeC
 text(-2, 104, "C", cex=2, xpd=TRUE)
 par(mfrow = c(1, 1))
 
-## ----rbarplot2, eval = TRUE----------------------------------------------
+## ----rbarplot2, eval = TRUE---------------------------------------------------
 ## barplot using Rcssplot, version 2 (using a default style and a compulsory class)
 rcss.barplot.2 <- function(x, main="Custom Rcss plot", ylab="y label", 
                            Rcss="default", Rcssclass=c()) {
@@ -199,11 +200,11 @@ rcss.barplot.2 <- function(x, main="Custom Rcss plot", ylab="y label",
   mtext(ylab, side=2, Rcssclass="ylab")
 }
 
-## ----global, eval=FALSE--------------------------------------------------
+## ----global, eval=FALSE-------------------------------------------------------
 #  RcssDefaultStyle <- style3
 #  RcssCompulsoryClass <- c()
 
-## ----rbarplot3, eval = TRUE----------------------------------------------
+## ----rbarplot3, eval = TRUE---------------------------------------------------
 ## barplot using Rcssplot, version 3 (assumes global style)
 rcss.barplot.3 <- function(x, main="Custom Rcss plot", ylab="y label", 
                            Rcssclass="typeB") {
@@ -218,11 +219,11 @@ rcss.barplot.3 <- function(x, main="Custom Rcss plot", ylab="y label",
   mtext(ylab, side=2, Rcssclass="ylab")
 }
 
-## ----reset, eval=TRUE----------------------------------------------------
+## ----reset, eval=TRUE---------------------------------------------------------
 RcssDefaultStyle <- NULL
 RcssCompulsoryClass <- NULL
 
-## ----snippet1------------------------------------------------------------
+## ----snippet1-----------------------------------------------------------------
 RcssCompulsoryClass <- "bar0"
 RcssCompulsoryClass
 foo1 <- function() {
@@ -231,7 +232,7 @@ foo1 <- function() {
 }
 foo1()
 
-## ----snippet2------------------------------------------------------------
+## ----snippet2-----------------------------------------------------------------
 foo2 <- function() {
    RcssCompulsoryClass <- RcssGetCompulsoryClass("bar2")
    RcssCompulsoryClass
@@ -239,18 +240,18 @@ foo2 <- function() {
 foo2()
 RcssCompulsoryClass
 
-## ----barrcss_4, eval=TRUE, echo = FALSE, comment=NA----------------------
+## ----barrcss_4, eval=TRUE, echo = FALSE, comment=NA---------------------------
 showrcss("Rcss/vignettes.bar4.Rcss")
 
-## ----getprop, eval=TRUE--------------------------------------------------
+## ----getprop, eval=TRUE-------------------------------------------------------
 style4 <- Rcss(file.path("Rcss", paste0("vignettes.bar", c(1, 2, 4), ".Rcss")))
 RcssProperty("baraxis", "stripe", Rcss=style4)
 
-## ----getprop2------------------------------------------------------------
+## ----getprop2-----------------------------------------------------------------
 RcssValue("baraxis", "stripe", default=0, Rcss=style4)
 RcssValue("baraxis", "strpe", default=0, Rcss=style4)
 
-## ----barplot6, eval=TRUE-------------------------------------------------
+## ----barplot6, eval=TRUE------------------------------------------------------
 ## barplot using Rcssplot, version 5 (uses custom css selectors)
 rcss.barplot.4 <- function(x, main="", ylab="Proportion (%)", 
                            Rcss="default", Rcssclass=c()) {
@@ -278,13 +279,13 @@ rcss.barplot.4 <- function(x, main="", ylab="Proportion (%)",
   mtext(ylab, side=2, Rcssclass="ylab")
 }
 
-## ----stripeA, eval = FALSE-----------------------------------------------
+## ----stripeA, eval = FALSE----------------------------------------------------
 #  rcss.barplot.4(a, main="Stripes", Rcss=style4)
 
-## ----stripeB, eval = FALSE-----------------------------------------------
+## ----stripeB, eval = FALSE----------------------------------------------------
 #  rcss.barplot.4(a, main="Stripes, y-scale 100", Rcss=style4, Rcssclass="dotted")
 
-## ----stripeC, eval = FALSE-----------------------------------------------
+## ----stripeC, eval = FALSE----------------------------------------------------
 #  a2 <- setNames(c(12, 20, 26, 72, 88, 94), tail(letters, 6))
 #  rcss.barplot.4(a2, main="... new data", Rcss=style4, Rcssclass="dotted")
 
@@ -299,22 +300,22 @@ rcss.barplot.4(a2, main="... new data", Rcss=style4, Rcssclass="dotted")
 graphics::text(-2, 130, "C", cex=2, xpd=TRUE)
 par(mfrow = c(1, 1))
 
-## ----watching, eval = FALSE----------------------------------------------
+## ----watching, eval = FALSE---------------------------------------------------
 #  style.files = file.path("Rcss", paste0("vignettes.bar", c(1, 2, 3), ".Rcss"))
 #  RcssWatch("rcss.barplot.4", files=style.files, x=a)
 
-## ----positional.error, eval = FALSE--------------------------------------
+## ----positional.error, eval = FALSE-------------------------------------------
 #  barplot(a, 2)
 
-## ----positional.solution, eval=FALSE-------------------------------------
+## ----positional.solution, eval=FALSE------------------------------------------
 #  graphics::barplot(a, 2)
 
-## ----positional.solution2, eval=FALSE------------------------------------
+## ----positional.solution2, eval=FALSE-----------------------------------------
 #  barplot(a, width=2)
 
-## ----grammar, eval=TRUE, echo = FALSE, comment=NA------------------------
+## ----grammar, eval=TRUE, echo = FALSE, comment=NA-----------------------------
 showrcss("Rcss/grammar.txt")
 
-## ------------------------------------------------------------------------
+## ----session_info-------------------------------------------------------------
 sessionInfo()
 
